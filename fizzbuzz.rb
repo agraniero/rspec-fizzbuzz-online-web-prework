d9d7e57e71fe8d15412a3173e2d6b24db69bf314
Anthony Graniero <ananfro@gmail.com>
2018-10-13 18:33:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@ def fizzbuzz(n)
   if (n%3 == 0) && (n%5 != 0)
 		return "Fizz"
 
-	elsif n%5 == 0 && n%3 != 0
-		puts "Buzz"
+	elsif (n%5 == 0) && (n%3 != 0)
+		return "Buzz"
 
-  elsif n%3 == 0 && n%5 == 0
-    puts "FizzBuzz"
+  elsif (n%3 == 0) && (n%5 == 0)
+    return "FizzBuzz"
   
-  else puts nil
+  else return nil
 end
